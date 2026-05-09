bdc8d2f25a3c6d92d037f51588187fcefaacbaea
Varun Gangadharan <varunpg2@illinois.edu>
2026-05-09 19:49:22 -0400

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc";
 
-  export default defineConfig({
-    plugins: [react()],
-    base: "/",
-  });
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    watch: {
+      ignored: ["**/_old_site/**", "**/.git/**"],
+    },
+  },
+  base: "/",
+});
