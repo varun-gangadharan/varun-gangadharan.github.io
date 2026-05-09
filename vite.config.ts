a9de2c885754b9a717e4e7f1fdd376b3953e1ef2
Varun Gangadharan <varunpg2@illinois.edu>
2026-05-09 19:56:18 -0400

    Fix GitHub Pages runtime deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,22 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: "app.html",
+      output: {
+        inlineDynamicImports: true,
+        entryFileNames: "site-assets/app.js",
+        chunkFileNames: "site-assets/[name].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith(".css")) {
+            return "site-assets/app.css";
+          }
+
+          return "site-assets/[name][extname]";
+        },
+      },
+    },
   },
   server: {
     watch: {
